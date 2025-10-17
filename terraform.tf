12dafb421b6f8d2ca2d0293b8fab5fbf0593d8c7
Sai Sumanvi Tummala <sumanvi.tummala2004@gmail.com>
2025-10-17 13:37:26 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -65,13 +65,9 @@ resource "aws_instance" "my_ec2" {
   }
 }
 
-# Elastic IP
-resource "aws_eip" "web_eip" {
-  instance   = aws_instance.my_ec2.id
-  depends_on = [aws_instance.my_ec2]
+
+# Output public IP of the EC2 instance
+output "instance_public_ip" {
+  value = aws_instance.my_ec2.public_ip
 }
 
-# Output public IP
-output "instance_public_ip" {
-  value = aws_eip.web_eip.public_ip
-}
